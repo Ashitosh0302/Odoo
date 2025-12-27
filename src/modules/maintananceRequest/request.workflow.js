8f6ff99ea4fe45f166e3bd6331cdff17242675b5
Ashitosh Shirsath <ashitoshshirsath9a62@gmail.com>
2025-12-27 13:15:19 +0500

    Update request.workflow.js

--- src/modules/maintananceRequest/request.workflow.js
+++ src/modules/maintananceRequest/request.workflow.js
@@ -1,1 +1,16 @@
+import Equipment from "../equipment/equipment.model.js";
+
+const AUTO_FILL_FROM_EQUIPMENT = async (request) =>
+{
+    const equipment = await Equipment.findById(request.equipment);
+
+    request.maintenanceTeam = equipment.maintenanceTeam;
+
+    return request;
+};
+
+export
+{
+    AUTO_FILL_FROM_EQUIPMENT
+};
 
