f3d36a18543d4fba0b84dd19ff18ee995898cffb
Ashitosh Shirsath <ashitoshshirsath9a62@gmail.com>
2025-12-27 12:54:11 +0500

    Update team.model.js

--- src/modules/maintananceTeam/team.model.js
+++ src/modules/maintananceTeam/team.model.js
@@ -1,1 +1,22 @@
+const mongoose = require("mongoose");
+
+const MaintenanceTeamSchema = new mongoose.Schema(
+{
+    name:
+    {
+        type: String,
+        required: true
+    },
+    members:
+    [
+        {
+            type: String
+        }
+    ]
+},
+{
+    timestamps: true
+});
+
+module.exports = mongoose.model("MaintenanceTeam", MaintenanceTeamSchema);
 
