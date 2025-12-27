31b33b257aaa692d911b0de2753fa052b2141fe9
Ashitosh Shirsath <ashitoshshirsath9a62@gmail.com>
2025-12-27 13:12:58 +0500

    Update request.controller.js

--- src/modules/maintananceRequest/request.controller.js
+++ src/modules/maintananceRequest/request.controller.js
@@ -1,1 +1,28 @@
+import * as SERVICE from "./request.service.js";
+
+const CREATE = async (req, res) =>
+{
+    const request = await SERVICE.CREATE_REQUEST(req.body);
+    res.status(201).json(request);
+};
+
+const UPDATE_STATUS = async (req, res) =>
+{
+    const { status, duration } = req.body;
+    const request = await SERVICE.UPDATE_STATUS(req.params.id, status, duration);
+    res.json(request);
+};
+
+const KANBAN = async (req, res) =>
+{
+    const data = await SERVICE.GET_KANBAN();
+    res.json(data);
+};
+
+export
+{
+    CREATE,
+    UPDATE_STATUS,
+    KANBAN
+};
 
