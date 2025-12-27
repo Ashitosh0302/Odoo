93e256e578ad1853875b7a33b394be63937d1abb
Ashitosh Shirsath <ashitoshshirsath9a62@gmail.com>
2025-12-27 13:13:23 +0500

    Update request.model.js

--- src/modules/maintananceRequest/request.model.js
+++ src/modules/maintananceRequest/request.model.js
@@ -1,1 +1,51 @@
+import mongoose from "mongoose";
+
+const RequestSchema = new mongoose.Schema(
+{
+    subject:
+    {
+        type: String,
+        required: true
+    },
+    type:
+    {
+        type: String,
+        enum: ["Corrective", "Preventive"],
+        required: true
+    },
+    equipment:
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Equipment",
+        required: true
+    },
+    maintenanceTeam:
+    {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "MaintenanceTeam"
+    },
+    assignedTechnician:
+    {
+        type: String
+    },
+    scheduledDate:
+    {
+        type: Date
+    },
+    duration:
+    {
+        type: Number
+    },
+    status:
+    {
+        type: String,
+        enum: ["New", "In Progress", "Repaired", "Scrap"],
+        default: "New"
+    }
+},
+{
+    timestamps: true
+});
+
+export default mongoose.model("MaintenanceRequest", RequestSchema);
 
