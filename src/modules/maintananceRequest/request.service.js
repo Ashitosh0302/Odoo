7743135b4852fe90248b60f6272c1461a3f2726c
Ashitosh Shirsath <ashitoshshirsath9a62@gmail.com>
2025-12-27 13:14:13 +0500

    Update request.service.js

--- src/modules/maintananceRequest/request.service.js
+++ src/modules/maintananceRequest/request.service.js
@@ -1,1 +1,41 @@
+import Request from "./request.model.js";
+import * as WORKFLOW from "./request.workflow.js";
+import Equipment from "../equipment/equipment.model.js";
+
+const CREATE_REQUEST = async (data) =>
+{
+    data = await WORKFLOW.AUTO_FILL_FROM_EQUIPMENT(data);
+    return await Request.create(data);
+};
+
+const UPDATE_STATUS = async (id, status, duration) =>
+{
+    const request = await Request.findByIdAndUpdate(
+        id,
+        { status, duration },
+        { new: true }
+    );
+
+    if (status === "Scrap")
+    {
+        await Equipment.findByIdAndUpdate(
+            request.equipment,
+            { isScrapped: true }
+        );
+    }
+
+    return request;
+};
+
+const GET_KANBAN = async () =>
+{
+    return await Request.find().populate("equipment maintenanceTeam");
+};
+
+export
+{
+    CREATE_REQUEST,
+    UPDATE_STATUS,
+    GET_KANBAN
+};
 
