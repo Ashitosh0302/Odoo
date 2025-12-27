f7b2572235065b957299e10f2e32980d3b4d1c6a
Ashitosh Shirsath <ashitoshshirsath9a62@gmail.com>
2025-12-27 13:02:00 +0500

    Update team.model.js to type module

--- src/modules/maintananceTeam/team.model.js
+++ src/modules/maintananceTeam/team.model.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const MaintenanceTeamSchema = new mongoose.Schema(
 {
@@ -18,5 +18,4 @@ const MaintenanceTeamSchema = new mongoose.Schema(
     timestamps: true
 });
 
-module.exports = mongoose.model("MaintenanceTeam", MaintenanceTeamSchema);
-
+export default mongoose.model("MaintenanceTeam", MaintenanceTeamSchema);
