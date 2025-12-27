d128e27bbd002b0548c4532e0737ba36eb1356e7
Ashitosh Shirsath <ashitoshshirsath9a62@gmail.com>
2025-12-27 13:13:50 +0500

    Update request.routes.js

--- src/modules/maintananceRequest/request.routes.js
+++ src/modules/maintananceRequest/request.routes.js
@@ -1,1 +1,11 @@
+import express from "express";
+import * as controller from "./request.controller.js";
+
+const router = express.Router();
+
+router.post("/", controller.CREATE);
+router.get("/kanban", controller.KANBAN);
+router.patch("/:id/status", controller.UPDATE_STATUS);
+
+export default router;
 
