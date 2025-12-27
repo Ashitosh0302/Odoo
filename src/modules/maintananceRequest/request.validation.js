df7e8e1c04d8ffd5d26f16ccaee3fe0341f44108
Ashitosh Shirsath <ashitoshshirsath9a62@gmail.com>
2025-12-27 13:14:41 +0500

    Update request.validation.js

--- src/modules/maintananceRequest/request.validation.js
+++ src/modules/maintananceRequest/request.validation.js
@@ -1,1 +1,59 @@
+import Joi from "joi";
+
+const CREATE_REQUEST = Joi.object(
+{
+    subject:
+    Joi.string()
+        .min(3)
+        .max(200)
+        .required(),
+
+    type:
+    Joi.string()
+        .valid("Corrective", "Preventive")
+        .required(),
+
+    equipment:
+    Joi.string()
+        .required(),
+
+    scheduledDate:
+    Joi.when("type",
+    {
+        is: "Preventive",
+        then: Joi.date().required(),
+        otherwise: Joi.forbidden()
+    }),
+
+    assignedTechnician:
+    Joi.string()
+        .optional(),
+
+    status:
+    Joi.string()
+        .valid("New", "In Progress", "Repaired", "Scrap")
+        .optional()
+});
+
+const UPDATE_STATUS = Joi.object(
+{
+    status:
+    Joi.string()
+        .valid("In Progress", "Repaired", "Scrap")
+        .required(),
+
+    duration:
+    Joi.when("status",
+    {
+        is: "Repaired",
+        then: Joi.number().positive().required(),
+        otherwise: Joi.forbidden()
+    })
+});
+
+export
+{
+    CREATE_REQUEST,
+    UPDATE_STATUS
+};
 
