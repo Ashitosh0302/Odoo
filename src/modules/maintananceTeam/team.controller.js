de9ebc3cd6394ad0eaf7b075e55a792d4dd3b4da
Ashitosh Shirsath <ashitoshshirsath9a62@gmail.com>
2025-12-27 12:55:13 +0500

    Update team.controller.js

--- src/modules/maintananceTeam/team.controller.js
+++ src/modules/maintananceTeam/team.controller.js
@@ -1,1 +1,20 @@
+const SERVICE = require("./team.service");
+
+const CREATE = async(req, res) =>
+{
+    const team = await SERVICE.CREATE_TEAM(req.body);
+    res.status(201).json(team);
+};
+
+const GET_ALL = async(req, res) =>
+{
+    const teams = await SERVICE.GET_ALL_TEAMS();
+    res.json(teams);
+};
+
+module.exports =
+{
+    CREATE,
+    GET_ALL
+};
 
