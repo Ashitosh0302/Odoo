6818f58b7ef143b923f8cfaabffe57406965e659
Ashitosh Shirsath <ashitoshshirsath9a62@gmail.com>
2025-12-27 12:56:29 +0500

    Update team.service.js

--- src/modules/maintananceTeam/team.service.js
+++ src/modules/maintananceTeam/team.service.js
@@ -1,1 +1,18 @@
+const Team = require("./team.model");
+
+const CREATE_TEAM = async(data) =>
+{
+    return await Team.create(data);
+};
+
+const GET_ALL_TEAMS = async() =>
+{
+    return await Team.find();
+};
+
+module.exports =
+{
+    CREATE_TEAM,
+    GET_ALL_TEAMS
+};
 
