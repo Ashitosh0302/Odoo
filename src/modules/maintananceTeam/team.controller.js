e6f8166e2efe6e6783b53864e3400c2320c3e00e
Ashitosh Shirsath <ashitoshshirsath9a62@gmail.com>
2025-12-27 13:01:25 +0500

    Update team.controller.js to type module

--- src/modules/maintananceTeam/team.controller.js
+++ src/modules/maintananceTeam/team.controller.js
@@ -1,20 +1,19 @@
-const SERVICE = require("./team.service");
+import * as SERVICE from "./team.service.js";
 
-const CREATE = async(req, res) =>
+const CREATE = async (req, res) =>
 {
     const team = await SERVICE.CREATE_TEAM(req.body);
     res.status(201).json(team);
 };
 
-const GET_ALL = async(req, res) =>
+const GET_ALL = async (req, res) =>
 {
     const teams = await SERVICE.GET_ALL_TEAMS();
     res.json(teams);
 };
 
-module.exports =
+export
 {
     CREATE,
     GET_ALL
 };
-
