feda4a24579e7c0ce14852cfc6ec791661fb1f56
Ashitosh Shirsath <ashitoshshirsath9a62@gmail.com>
2025-12-27 13:02:30 +0500

    Update team.routes.js

--- src/modules/maintananceTeam/team.routes.js
+++ src/modules/maintananceTeam/team.routes.js
@@ -1,1 +1,10 @@
+import express from "express";
+import * as controller from "./team.controller.js";
+
+const router = express.Router();
+
+router.post("/", controller.CREATE);
+router.get("/", controller.GET_ALL);
+
+export default router;
 
